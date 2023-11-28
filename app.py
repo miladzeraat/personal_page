9cc65b87f49db0ac5b4f5c48dee9b6287404b107
Milad <mi.zeraat@gmail.com>
2023-11-28 16:50:30 +0100

    adding my_app page

--- app.py
+++ app.py
@@ -5,6 +5,9 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('index.html')
+@app.route('/myApps')  
+def my_apps():
+    return render_template('my_apps.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
